83bc9da610fe917bdbdaac67210852a2b7295d1e
weicno <naicfeng@gmail.com>
2018-11-21 22:03:00 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -5,8 +5,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package GAuthenticator
  * @author WeiCN
- * @version 0.0.1
- * @link https://weicn.org
+ * @version 0.0.2
+ * @link https://cuojue.org/read/Typecho_Google_Authenticator_02.html
  */
 class GAuthenticator_Plugin implements Typecho_Plugin_Interface
 {
@@ -20,10 +20,9 @@ class GAuthenticator_Plugin implements Typecho_Plugin_Interface
      */
     public static function activate()
     {
+		Helper::addRoute('GAuthenticator', '/GAuthenticator', 'GAuthenticator_Action', 'Action');
 		Typecho_Plugin::factory('admin/menu.php')->navBar = array(__CLASS__, 'Authenticator_safe');
-		Typecho_Plugin::factory('admin/header.php')->header = array(__CLASS__, 'Authenticator_verification');
-		Typecho_Plugin::factory('Widget_Login')->loginFail = array(__CLASS__, 'Authenticator_login');
-		Typecho_Plugin::factory('Widget_Login')->loginSucceed = array(__CLASS__, 'Authenticator_loginSucceed');
+		Typecho_Plugin::factory('admin/common.php')->begin = array(__CLASS__, 'Authenticator_verification');
 		return _t('当前两步验证还未启用，请进行<a href="options-plugin.php?config=' . self::$pluginName . '">初始化设置</a>');
     }
     
@@ -48,7 +47,7 @@ class GAuthenticator_Plugin implements Typecho_Plugin_Interface
     {
 		$element = new Typecho_Widget_Helper_Form_Element_Text('SecretKey', NULL, '', _t('SecretKey'), '安装的时候自动计算密钥,手动修改无效,如需要修改请卸载重新安装或者手动修改数据库');
 		$form->addInput($element);
-		$element = new Typecho_Widget_Helper_Form_Element_Text('SecretQRurl', NULL, '', _t('二维码的网址'), '复制打开就可以显示当前SecretKey的二维码,扫描就可以绑定,调用的GoogleAPI,如果无法显示可能是被墙了');
+		$element = new Typecho_Widget_Helper_Form_Element_Text('SecretQRurl', NULL, '', _t('二维码的网址'), '复制打开就可以显示当前SecretKey的二维码,扫描就可以绑定');
 		$form->addInput($element);
 		$element = new Typecho_Widget_Helper_Form_Element_Text('SecretTime', NULL, 2, _t('容差时间'), '允许的容差时间,单位为30秒的倍数,如果这里是2 那么就是 2* 30 sec 一分钟.');
 		$form->addInput($element);
@@ -68,7 +67,7 @@ class GAuthenticator_Plugin implements Typecho_Plugin_Interface
 			require_once 'GoogleAuthenticator.php';
 			$Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
 			$config['SecretKey'] = $Authenticator->createSecret();//生成一个随机安全密钥
-			$config['SecretQRurl'] = $Authenticator->getQRCodeGoogleUrl(Helper::options()->title, $config['SecretKey']);//生成安全密钥的二维码网址
+			$config['SecretQRurl'] = 'http://qr.liantu.com/api.php?text='.urlencode('otpauth://totp/'.Helper::options()->title.'?secret='.$config['SecretKey']);//生成安全密钥的二维码网址
 		}else{
 			$config_old = Helper::options()->plugin(self::$pluginName);
 			if(($config['SecretCode']!='' && $config['SecretOn']==1) || $config['SecretOn']==1){//如果启用,并且验证码不为空
@@ -111,56 +110,24 @@ class GAuthenticator_Plugin implements Typecho_Plugin_Interface
 		}
     }
 
-    public static function Authenticator_verification($header)
+    public static function Authenticator_verification()
     {
-		if (Typecho_Widget::widget('Widget_User')->hasLogin()) return $header;//如果已经登录则直接返回
-		$config = Helper::options()->plugin(self::$pluginName);
-		if (Typecho_Cookie::get('__typecho_Authenticator') == md5($config->SecretKey.Typecho_Cookie::getPrefix())) return $header;//如果COOKIES匹配则直接返回
-		if($config->SecretOn==1){
-			$options = Helper::options();
-			$request = new Typecho_Request();
-			require_once 'verification.php';
+		if(isset($Authenticator_init))return;
+		$Authenticator_init = true;
+		if (!Typecho_Widget::widget('Widget_User')->hasLogin()){
+			return;//如果没登录则直接返回
 		}else{
-			return $header;//如果未开启插件则直接返回
-		}
-    }
-
-	public static function Authenticator_login($user, $name, $password, $remember)
-    {
-		$config = Helper::options()->plugin(self::$pluginName);
-		$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'javascript:history.back(-1);';
-		if($config->SecretOn==1 && Typecho_Cookie::get('__typecho_Authenticator') != md5($config->SecretKey.Typecho_Cookie::getPrefix())){//如果插件启用且验证不匹配
-			if($name=='_AuthenticatorCode'){
-				require_once 'GoogleAuthenticator.php';
-				$Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
-				//$oneCode = $Authenticator->getCode($secret);//服务器生成的一次性代码
-				$oneCode = $password;//手机端生成的一次性代码
-				if($Authenticator->verifyCode($config->SecretKey, $oneCode, $config->SecretTime)){//验证一次性代码
-					Typecho_Cookie::set('__typecho_Authenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix()));//由于typecho先判断在线状态才启用的session,所以我们用cookies来保存
-					Typecho_Widget::widget('Widget_Notice')->set(_t('两步验证通过,请使用密码登录'), 'success');
-					Typecho_Response::redirect($referer);
-				}else{
-					self::showErrorMsg('两步验证失败');
-				}
+			//已经登录就验证
+			$config = Helper::options()->plugin(self::$pluginName);
+			if (isset($_SESSION['GAuthenticator'])&&$_SESSION['GAuthenticator']) return;//如果SESSION匹配则直接返回
+			if (Typecho_Cookie::get('__typecho_GAuthenticator') == md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid)) return;//如果COOKIE匹配则直接返回
+			if($config->SecretOn==1){
+				$options = Helper::options();
+				$request = new Typecho_Request();
+				require_once 'verification.php';
 			}else{
-				self::showErrorMsg('未通过两步验证，禁止登录');//如果强行POST登录则拦截
-				Typecho_Response::redirect($referer);
+				return;//如果未开启插件则直接返回
 			}
-		}elseif(Typecho_Cookie::get('__typecho_Authenticator') == md5($config->SecretKey.Typecho_Cookie::getPrefix())){
-			Typecho_Widget::widget('Widget_Login')->login($name, $password, false, $remember);//调用系统登录接口
 		}
-    }
-	public static function Authenticator_loginSucceed($user, $name, $password, $remember)
-	{
-		Typecho_Cookie::delete('__typecho_Authenticator');//登录成功后删掉验证用的COOKIES
-	}
-    /**
-     *显示错误信息并跳回登录界面
-     *@return void
-     */
-    public static function showErrorMsg($msg) {
-        $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'javascript:history.back(-1);';
-        Typecho_Widget::widget('Widget_Notice')->set(_t($msg), 'error');
-        Typecho_Response::redirect($referer);
     }
 }

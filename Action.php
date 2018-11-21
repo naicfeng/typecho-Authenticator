9c605a3a8c7254091f171455dc9befa4b7645ca0
weicno <naicfeng@gmail.com>
2018-11-22 00:03:41 +0800

    忘记删除调试代码

--- Action.php
+++ Action.php
@@ -18,28 +18,27 @@ class GAuthenticator_Action extends Typecho_Widget implements Widget_Interface_D
 	 * 
 	 */
 	public function auth(){
-        var_dump($this->request->get('remember'));
-        if(intval($this->request->get('otp'))>0){
-            //获取到CODE
-            $config = Helper::options()->plugin('GAuthenticator');
-            require_once 'GoogleAuthenticator.php';
-            $referer = $this->request->getReferer();
-            $Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
-            $oneCode = intval($this->request->get('otp'));//手机端生成的一次性代码
-            if($Authenticator->verifyCode($config->SecretKey, $oneCode, $config->SecretTime)){//验证一次性代码
-                $expire = 1 == $this->request->get('remember') ? Helper::options()->time + Helper::options()->timezone + 30*24*3600 : 0;
-                $_SESSION['GAuthenticator'] = true;//session保存
-                Typecho_Cookie::set('__typecho_GAuthenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid),$expire);//cookie保存
-            }else{
-                Typecho_Widget::widget('Widget_Notice')->set(_t('两步验证失败'), 'error');
-            }
-            Typecho_Response::redirect($referer);
-        }
+		if(intval($this->request->get('otp'))>0){
+			//获取到CODE
+			$config = Helper::options()->plugin('GAuthenticator');
+			require_once 'GoogleAuthenticator.php';
+			$referer = $this->request->getReferer();
+			$Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
+			$oneCode = intval($this->request->get('otp'));//手机端生成的一次性代码
+			if($Authenticator->verifyCode($config->SecretKey, $oneCode, $config->SecretTime)){//验证一次性代码
+				$expire = 1 == $this->request->get('remember') ? Helper::options()->time + Helper::options()->timezone + 30*24*3600 : 0;
+				$_SESSION['GAuthenticator'] = true;//session保存
+				Typecho_Cookie::set('__typecho_GAuthenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid),$expire);//cookie保存
+			}else{
+				Typecho_Widget::widget('Widget_Notice')->set(_t('两步验证失败'), 'error');
+			}
+			Typecho_Response::redirect($referer);
+		}
 	}
 
 	public function action(){
-        $this->widget('Widget_User')->pass('administrator');
-        $this->on($this->request->is('otp'))->auth();
+		$this->widget('Widget_User')->pass('administrator');
+		$this->on($this->request->is('otp'))->auth();
 	}
 }
 ?>

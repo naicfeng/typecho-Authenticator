26ff92ddb26a7bd6014b07bac2ddc8ddc84dae7a
weicno <naicfeng@gmail.com>
2018-11-21 22:03:57 +0800

    update 0.0.2

---
+++ Action.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * GAuthenticator Plugin
+ *
+ * @copyright  Copyright (c) 2018 WeiCN (https://cuojue.org)
+ * @license	GNU General Public License 2.0
+ * 
+ */
+class GAuthenticator_Action extends Typecho_Widget implements Widget_Interface_Do
+{
+
+	public function __construct($request, $response, $params = NULL)
+	{
+		parent::__construct($request, $response, $params);
+	}
+	/**
+	 * 验证GAuthenticator POST
+	 * 
+	 */
+	public function auth(){
+        if(intval($this->request->get('otp'))>0){
+            //获取到CODE
+            $config = Helper::options()->plugin('GAuthenticator');
+            require_once 'GoogleAuthenticator.php';
+            $referer = $this->request->getReferer();
+            $Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
+            $oneCode = intval($this->request->get('otp'));//手机端生成的一次性代码
+            if($Authenticator->verifyCode($config->SecretKey, $oneCode, $config->SecretTime)){//验证一次性代码
+                $_SESSION['GAuthenticator'] = true;//session保存
+                Typecho_Cookie::set('__typecho_GAuthenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid));//cookie保存
+            }else{
+                Typecho_Widget::widget('Widget_Notice')->set(_t('两步验证失败'), 'error');
+            }
+            Typecho_Response::redirect($referer);
+        }
+	}
+
+	public function action(){
+        $this->on($this->request->is('otp'))->auth();
+	}
+}
+?>

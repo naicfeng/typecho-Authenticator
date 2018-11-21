a1e5c2012cfdf38a3ced10f7c093ce478096c95c
weicno <naicfeng@gmail.com>
2018-11-21 23:58:42 +0800

    更新支持记住本机
    利用cookie的有效期记住本机一个月避免重复验证

--- Action.php
+++ Action.php
@@ -18,6 +18,7 @@ class GAuthenticator_Action extends Typecho_Widget implements Widget_Interface_D
 	 * 
 	 */
 	public function auth(){
+        var_dump($this->request->get('remember'));
         if(intval($this->request->get('otp'))>0){
             //获取到CODE
             $config = Helper::options()->plugin('GAuthenticator');
@@ -26,8 +27,9 @@ class GAuthenticator_Action extends Typecho_Widget implements Widget_Interface_D
             $Authenticator = new PHPGangsta_GoogleAuthenticator();//初始化生成类
             $oneCode = intval($this->request->get('otp'));//手机端生成的一次性代码
             if($Authenticator->verifyCode($config->SecretKey, $oneCode, $config->SecretTime)){//验证一次性代码
+                $expire = 1 == $this->request->get('remember') ? Helper::options()->time + Helper::options()->timezone + 30*24*3600 : 0;
                 $_SESSION['GAuthenticator'] = true;//session保存
-                Typecho_Cookie::set('__typecho_GAuthenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid));//cookie保存
+                Typecho_Cookie::set('__typecho_GAuthenticator',md5($config->SecretKey.Typecho_Cookie::getPrefix().Typecho_Widget::widget('Widget_User')->uid),$expire);//cookie保存
             }else{
                 Typecho_Widget::widget('Widget_Notice')->set(_t('两步验证失败'), 'error');
             }
@@ -36,6 +38,7 @@ class GAuthenticator_Action extends Typecho_Widget implements Widget_Interface_D
 	}
 
 	public function action(){
+        $this->widget('Widget_User')->pass('administrator');
         $this->on($this->request->is('otp'))->auth();
 	}
 }
